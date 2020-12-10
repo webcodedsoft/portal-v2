5dc941784340ed93a80e6621874d12f05fa33dee
Adedeji Sikiru <adedejisikiruadesola@gmail.com>
2020-12-10 11:23:20 +0100

    Add Ignore file and DB Connection

--- function/dbconfig.php
+++ function/dbconfig.php
@@ -1,13 +1,13 @@
 <?php
-/*$con = mysqli_connect("localhost","assanusi_websoft","webcoded1","assanusi_asscoedportal");
+$con = mysqli_connect("localhost", "asscoed_webcoded","webcoded1","asscoed_portal");
 if(mysqli_connect_errno($con))
 {
 	echo mysqli_connect_error();
 }
-*/
-  $con = mysqli_connect("localhost","root","webcoded1","new_college_db");
-if(mysqli_connect_errno($con))
-{
-	echo mysqli_connect_error();
-}
+
+//   $con = mysqli_connect("localhost","root","webcoded1","new_college_db");
+// if(mysqli_connect_errno($con))
+// {
+// 	echo mysqli_connect_error();
+// }
 ?>
